8c1e5223f2fa7f11aa96be68ef0307b6e413186d
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-07-15 14:26:07 +0300

    Added 03.bubble-sort

---
+++ 03.Searching-and-Sorting-Algorithms/problems/02.bubble-sort.py
@@ -0,0 +1,20 @@
+# 3. Bubble Sort
+
+def bubble_sort(nums):
+    is_sorted = False
+    sorted_cells = 0
+
+    while not is_sorted:
+        is_sorted = True
+        for index in range(1, len(nums) - sorted_cells):
+            if nums[index] < nums[index - 1]:
+                nums[index], nums[index - 1] = nums[index - 1], nums[index]
+                is_sorted = False
+        sorted_cells += 1
+
+
+nums = [int(x) for x in input().split()]
+
+bubble_sort(nums)
+
+print(*nums, sep=' ')
