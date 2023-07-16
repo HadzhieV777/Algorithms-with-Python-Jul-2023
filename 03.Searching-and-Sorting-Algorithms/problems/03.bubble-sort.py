963c12580d396ec7a72a5d07f8ed5b1d1848a1ef
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-07-16 09:49:48 +0300

    Edited 03.bubble-sort

--- 03.Searching-and-Sorting-Algorithms/problems/03.bubble-sort.py
+++ 03.Searching-and-Sorting-Algorithms/problems/03.bubble-sort.py
@@ -1,5 +1,11 @@
 # 3. Bubble Sort
 
+def bubble_sort_with_nested_cycles(nums):
+    for i in range(len(nums)):
+        for j in range(1, len(nums) - i):
+            if nums[j - 1] > nums[j]:
+            nums[j], nums[j - 1] = nums[j - 1], nums[j]
+
 def bubble_sort(nums):
     is_sorted = False
     sorted_cells = 0
